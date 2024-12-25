bc6e3e020ff56431e7dc083733436610c4c76802
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-12-25 17:10:41 +0530

    final commit

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
-from setuptools import find_packages,setup
-from typing import List
+# from setuptools import find_packages,setup
+# from typing import List
 
 
 
 
-HYPEN_E_DOT="-e."
+# HYPEN_E_DOT="-e."
 
 
 
-def get_requirements(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path, 'r') as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+# def get_requirements(file_path:str)->List[str]:
+#     requirements = []
+#     with open(file_path, 'r') as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","")for req in requirements]
 
-        return requirements
+#         return requirements
 
-        if HYPEN_E_DOT in requirment:
-            requirment.remove("HYPEN_E_DOT")
+#         if HYPEN_E_DOT in requirment:
+#             requirment.remove("HYPEN_E_DOT")
 
 
-setup(
-    name="DATA SCIENCE 101 CHALEGES",
-    version="0.0.1",
-    author="Abhishek Upadhyay",
-    author_email="abhishek.it21-25@recabn.ac.in",
-    install_requires=get_requirements("requirements.txt"),
-    packages=find_packages(),
+# setup(
+#     name="DATA SCIENCE 101 CHALEGES",
+#     version="0.0.1",
+#     author="Abhishek Upadhyay",
+#     author_email="abhishek.it21-25@recabn.ac.in",
+#     install_requires=get_requirements("requirements.txt"),
+#     packages=find_packages(),
     
     
-)
+# )
